8f796d4e27180a4bc15885024a8bc3fb08982ff1
MewPK <milzaza123@gmail.com>
2023-06-06 23:50:00 +0700

    update : install class-validator and create dto

--- Level1/src/auth/auth.controller.ts
+++ Level1/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
@@ -10,7 +11,11 @@ export class AuthController{
        return this.authService.signin();
     }
     @Post("signup")
-    signup(@Req() req : Request){
+    signup(@Body() dto:AuthDto){    
+      console.log({
+        dto
+      });
+      
       return this.authService.signup();
     }
 
